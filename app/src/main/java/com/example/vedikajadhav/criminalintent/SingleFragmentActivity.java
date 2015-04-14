2a0a10c94c22a8f62d9f0ad8de61524e1eb09b7e
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-04-13 23:48:55 -0700

    BigNerd Ch22 dual pane only layouts working

--- app/src/main/java/com/example/vedikajadhav/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/vedikajadhav/criminalintent/SingleFragmentActivity.java
@@ -11,10 +11,15 @@ import android.support.v7.app.ActionBarActivity;
 public abstract class SingleFragmentActivity extends ActionBarActivity {
     protected abstract Fragment createFragment();
 
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        //setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
